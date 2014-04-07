eb8ac03ee7d9c6c855d6ca21e7f26c6d47a2fd99
Ben Cordier <bcordier@columnfivemedia.com>
2014-04-06 17:16:57 -0700

    Version 0.1.0
    Interactive functioning and rendering correct solutions, etc.

--- js/interactive.js
+++ js/interactive.js
@@ -26,7 +26,7 @@ var scenarioData = [
 					"ancillary-business": false,
 					"baseline-product": false
 				},
-				"aircraft-type": {
+				"aircraftType": {
 					"narrow-body": true,
 					"wide-body": false
 				},
@@ -35,7 +35,7 @@ var scenarioData = [
 					"wireless-streaming": true,
 					"ecommerce": true,
 					"hd": true,
-					"third-pary-apps": true,
+					"third-party-apps": true,
 					"ped-power": true,
 					"overhead": false,
 					"advertising": true,
@@ -51,14 +51,15 @@ var scenarioData = [
 					"web-email": false,
 					"personalization": false
 				},
-				"main-product": "eX1",
-				"addons" : [
-					{"name": "eXW", "values": ["wireless-streaming"]},
-					{"name": "OneMedia", "values": ["advertising"]},
-					{"name": "PTS", "values": ["proactive-maintenance"]},
-					{"name": "PAXUS", "values": ["usage-statistics"]},
-					{"name": "InFlight API", "values": ["third-party-apps"]}
-				]
+				"mainProduct": "eX1",
+				"description": "Most Advanced IFE Solution",
+				"addons" : {
+					"wireless-streaming": "eXW",
+					"advertising": "OneMedia",
+					"proactive-maintenance": "PTS",
+					"usage-statistics": "PAXUS",
+					"third-party-apps": "InFlight API"
+				}
 			}
 		},
 		{
@@ -80,7 +81,7 @@ var scenarioData = [
 					"ancillary-business": false,
 					"baseline-product": false
 				},
-				"aircraft-type": {
+				"aircraftType": {
 					"narrow-body": false,
 					"wide-body": true
 				},
@@ -89,7 +90,7 @@ var scenarioData = [
 					"wireless-streaming": false,
 					"ecommerce": true,
 					"hd": true,
-					"third-pary-apps": true,
+					"third-party-apps": true,
 					"ped-power": true,
 					"overhead": false,
 					"advertising": true,
@@ -105,16 +106,19 @@ var scenarioData = [
 					"web-email": true,
 					"personalization": true
 				},
-				"main-product": "eX3",
-				"addons" : [
-					{"name": "eXConnect", "values": ["web-email", "live-tv", "personalization"]},
-					{"name": "exTV", "values": ["live-tv"]},
-					{"name": "eXPhone", "values": ["voice-sms"]},
-					{"name": "OneMedia", "values": ["advertising"]},
-					{"name": "PTS", "values": ["technical-support"]},
-					{"name": "InFlight API", "values": ["third-party-apps"]},
-					{"name": "Voyager 3D", "values": ["moving-map"]}
-				]
+				"mainProduct": "eX3",
+				"description": "Most Advanced IFE Solution",
+				"addons" : {
+					"web-email": "eXConnect",
+					"live-tv": "eXConnect",
+					"personalization": "eXConnect",
+					"live-tv": "eXTV",
+					"voice-sms": "eXPhone",
+					"advertising": "OneMedia",
+					"technical-support": "PTS",
+					"third-party-apps": "InFlight API",
+					"moving-map": "Voyager 3D"
+				}
 			}
 		},
 		{
@@ -136,7 +140,7 @@ var scenarioData = [
 					"ancillary-business": true,
 					"baseline-product": false
 				},
-				"aircraft-type": {
+				"aircraftType": {
 					"narrow-body": false,
 					"wide-body": true
 				},
@@ -145,7 +149,7 @@ var scenarioData = [
 					"wireless-streaming": false,
 					"ecommerce": true,
 					"hd": true,
-					"third-pary-apps": true,
+					"third-party-apps": true,
 					"ped-power": false,
 					"overhead": false,
 					"advertising": true,
@@ -158,17 +162,18 @@ var scenarioData = [
 					"live-tv": false,
 					"voice-sms": false,
 					"technical-support": false,
-					"web-email": false,
+					"web-email": true,
 					"personalization": false
 				},
-				"main-product": "eXLite",
-				"addons" : [
-					{"name": "eXConnect", "values": ["web-email"]},
-					{"name": "OneMedia", "values": ["advertising"]},
-					{"name": "PAXUS", "values": ["usage-statistics"]},
-					{"name": "InFlight API", "values": ["third-party-apps"]},
-					{"name": "Voyager 3D", "values": ["moving-map"]}
-				]
+				"mainProduct": "eXLite",
+				"description": "Lightest Seatback Solution",
+				"addons" : {
+					"web-email": "eXConnect",
+					"advertising": "OneMedia",
+					"usage-statistics": "PAXUS",
+					"third-party-apps": "InFlight API",
+					"moving-map": "Voyager 3D"
+				}
 			}
 		},
 		{
@@ -190,7 +195,7 @@ var scenarioData = [
 					"ancillary-business": false,
 					"baseline-product": false
 				},
-				"aircraft-type": {
+				"aircraftType": {
 					"narrow-body": true,
 					"wide-body": false
 				},
@@ -199,7 +204,7 @@ var scenarioData = [
 					"wireless-streaming": true,
 					"ecommerce": true,
 					"hd": false,
-					"third-pary-apps": false,
+					"third-party-apps": false,
 					"ped-power": false,
 					"overhead": true,
 					"advertising": true,
@@ -215,13 +220,14 @@ var scenarioData = [
 					"web-email": false,
 					"personalization": false
 				},
-				"main-product": "eXO",
-				"addons" : [
-					{"name": "+Monitors", "values": ["avod"]},
-					{"name": "WAPs", "values": ["wireless-streaming"]},
-					{"name": "OneMedia", "values": ["advertising"]},
-					{"name": "Voyager 3D", "values": ["moving-map"]}
-				]
+				"mainProduct": "eXO",
+				"description": "New Smart Overhead",
+				"addons" : {
+					"avod": "+Monitors",
+					"wireless-streaming": "WAPs",
+					"advertising": "OneMedia",
+					"moving-map": "Voyager 3D"
+				}
 			}
 		},
 		{
@@ -243,7 +249,7 @@ var scenarioData = [
 					"ancillary-business": true,
 					"baseline-product": true
 				},
-				"aircraft-type": {
+				"aircraftType": {
 					"narrow-body": true,
 					"wide-body": false
 				},
@@ -252,7 +258,7 @@ var scenarioData = [
 					"wireless-streaming": true,
 					"ecommerce": false,
 					"hd": true,
-					"third-pary-apps": false,
+					"third-party-apps": false,
 					"ped-power": false,
 					"overhead": false,
 					"advertising": false,
@@ -268,11 +274,11 @@ var scenarioData = [
 					"web-email": true,
 					"personalization": false
 				},
-				"main-product": "eXW",
-				"addons" : [
-					{"name": "eXConnect", "values": ["web-email"]}
-				]
-
+				"mainProduct": "eXW",
+				"description": "Mobile Device Experience",
+				"addons" : {
+					"web-email": "eXConnect"
+				}
 			}
 		},
 		{
@@ -294,7 +300,7 @@ var scenarioData = [
 					"ancillary-business": false,
 					"baseline-product": true
 				},
-				"aircraft-type": {
+				"aircraftType": {
 					"narrow-body": false,
 					"wide-body": true
 				},
@@ -303,7 +309,7 @@ var scenarioData = [
 					"wireless-streaming": false,
 					"ecommerce": true,
 					"hd": false,
-					"third-pary-apps": false,
+					"third-party-apps": false,
 					"ped-power": false,
 					"overhead": false,
 					"advertising": true,
@@ -319,37 +325,15 @@ var scenarioData = [
 					"web-email": true,
 					"personalization": false
 				},
-				"main-product": "eXConnect",
-				"addons" : [
-					{"name": "eXTV", "values": ["live-tv"]},
-					{"name": "eXPhone", "values": ["voice-sms"]},
-					{"name": "OneMedia", "values": ["advertising"]}
-				]
+				"mainProduct": "eXConnect",
+				"description": "Some Product Description",
+				"addons" : {
+					"live-tv": "eXTV",
+					"voice-sms": "eXPhone",
+					"advertising": "OneMedia"
+				}
 			}
 		}];
-
-var solutionKey = [
-	{
-		"eX1": {
-			"addons": []
-		},
-		"eX3": {
-			"addons": []
-		},
-		"eXLite": {
-			"addons": []
-		},
-		"eXO": {
-			"addons": []
-		},
-		"eXW": {
-			"addons": []
-		},
-		"eXConnect": {
-			"addons": []
-		}
-	}
-];
 
 var Interactive = function (slide, data) {
 	var self = this;
@@ -359,7 +343,9 @@ var Interactive = function (slide, data) {
 		self.s3Data = [],
 		self.s4Data = [],
 		self.solution = [],
-		self.addons = ko.observableArray([]);
+		self.main = ko.observable(),
+		self.description = ko.observable(),
+		self.addons = ko.observableArray(),
 		self.solutionsData = data,
 		self.illusionData = {},
 		self.s2Inactive = [],
@@ -404,12 +390,17 @@ Interactive.prototype.prev = function (callback) {
 	//If Callback Function Exists, Execute It
 	typeof callback == 'function' ? callback.apply(this) : void(0);
 };
-// Restart Interactive
+// Clear Data & Restart Interactive
 Interactive.prototype.restart = function (callback) {
 	this.s2Data = [];
 	this.s3Data = [];
 	this.s4Data = [];
 	this.solution = [];
+	this.main(null);
+	this.addons([]);
+	//Reset Tiles
+	$('.selected').removeClass('selected');
+	$('.inactive').removeClass('inactive');
 	$('.active').removeClass('active');
 	$(this.slide).first().addClass('active');
 	self.index = 1;
@@ -508,8 +499,6 @@ Interactive.prototype.buildArray = function (slide, array, parent) {
 // Accepts slide (str), data location (str - related to where in the data object to search), limit (int - limits the number of selections)
 // array (arr - user selection array) and data (data object of possible solutions) 
 Interactive.prototype.filterData = function (slide, location, limit, array, data) {
-	console.log(slide + "\n" + location + "\n" + limit + "\n" + array + "\n" + data);
-	console.log("^^^^^^^FILTER INPUT^^^^^^^^^");
 	var data = data || this.solutionsData, patterns = [], solutions = []; 
 	for (var i = 0; i < data.length; i++) {
 		var valid = [];
@@ -522,19 +511,32 @@ Interactive.prototype.filterData = function (slide, location, limit, array, data
 			solutions.push(data[i]);
 		};
 	};
-	console.log(solutions);
-	console.log("^^^^^^^SOLUTIONS^^^^^^^^^");
 	return solutions;
 };
 
-Interactive.prototype.getFeatures = function (data) {
-	var location = 'features';
-	console.log();
+Interactive.prototype.getAddons = function (array, data) {
+	var location = 'addons', tempArr = [], newArr = [];
+	for (var i = 0; i < array.length; i++) {
+		$.each(data[0].solution[location], function (k, v) {
+			if (array[i] == k) {
+				if (tempArr.indexOf(v) < 0) {
+					tempArr.push(v);
+				};
+			};
+		});
+	};
+	//Iterate over tempArr values and set each as a name object
+	for (var i = 0; i < tempArr.length; i++) {
+		newArr.push({"name": tempArr[i]});
+	}
+	console.log(newArr);
+	return newArr;
 };
 
 //Instantiation & Initialization
 var interactive = new Interactive('.slide', scenarioData);
 	interactive.init();
+	ko.applyBindings(interactive);
 
 /*
 General Navigation Event Bindings
@@ -544,7 +546,7 @@ General Navigation Event Bindings
 $('.getStarted').on("click", function () {
 	interactive.next();
 });
-
+//Restart Interactive
 $('.restart').on("click", function () {
 	interactive.restart();
 });
@@ -569,14 +571,14 @@ $('#s3 .next, #s3 .toAircraft-Type').on("click", function (e) {
 		limit = 2;
 		//Second screen should not affect outcome
 		// interactive.s3Data = interactive.filterData('s3', 'background', limit, array, interactive.s2Data);
-		interactive.s3Data = interactive.s2Data;
 		interactive.next();
 });
 //Slide 4 Next
 $('#s4 .next, #s4 .toFeatures').on("click", function (e) {
 	var array = interactive.buildArray('s4', interactive.s4Data, false),
 		limit = 1;
-		interactive.s4Data = interactive.filterData('s4', 'aircraft-type', 1, array, interactive.s3Data);
+		//Handing in S2 Data (S3 Data undefined because no input to S3)
+		interactive.s4Data = interactive.filterData('s4', 'aircraftType', 1, array, interactive.s2Data);
 		interactive.s5Inactive = interactive.renderInactive('s5', interactive.s4Data, 'features')
 		interactive.next();
 });
@@ -584,10 +586,12 @@ $('#s4 .next, #s4 .toFeatures').on("click", function (e) {
 $('#s5 .next, #s5 .toSolution').on("click", function (e) {
 	var array = interactive.buildArray('s5', interactive.solution, false),
 		limit = null;
-
-		//TODO: Need to retrieve user selected features, map to addons, and push into ko.observableArray
 		interactive.solution = interactive.filterData('s5', 'features', null, array, interactive.s4Data);
-		console.log(interactive.solution);
+		//Set Main Solution Observable & Description Observable
+		interactive.main(interactive.solution[0].solution.mainProduct);
+		interactive.description(interactive.solution[0].solution.description);
+		//Get Addons & Push into Observable Array
+		interactive.addons(interactive.getAddons(array, interactive.solution));
 		interactive.next();
 });
 
@@ -604,7 +608,7 @@ $('#s2 .prev').on("click", function (e) {
 });
 //Slide 3 Previous
 $('#s3 .prev, #s3 .toObjective').on("click", function (e) {
-	// interactive.s3Data = [];
+	interactive.s3Data = [];
 	$('#s3 .button').removeClass('selected').removeClass('inactive');
 	interactive.prev();
 });
@@ -620,9 +624,17 @@ $('#s5 .prev, #s5 .toAircraft-Type').on("click", function (e) {
 	$('#s5 .button').removeClass('selected').removeClass('inactive');
 	interactive.prev();
 });
+//Slide 6 Previous
+$('#s6 .prev, #s6 .toFeatures').on("click", function (e) {
+	//Reset Solution Data
+	interactive.solution = [];
+	interactive.main();
+	interactive.addons([]);
+	interactive.prev();
+});
 
 /*
-User Input Selection - Slide Specific
+User Selection Event Bindings - Slide Specific
 */
 
 //Access Slide 2 Inputs
